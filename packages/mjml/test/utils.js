faf57dabaf7ab53de1aff163751a65d0ac5b3058
Daz Greer <daz@darrengreer.co.uk>
2026-02-27 16:48:46 +0100

    Convert extractStyle to ES module export

--- packages/mjml/test/utils.js
+++ packages/mjml/test/utils.js
@@ -1,12 +1,9 @@
-function extractStyle(style, prop) {
+// eslint-disable-next-line import/prefer-default-export
+export function extractStyle(style, prop) {
   const start = style.indexOf(`${prop}:`) + prop.length + 1
   const end = style.indexOf(';', start)
   if (end === -1) {
     return style.substring(start).trim()
   }
   return style.substring(start, end).trim()
-}
-
-module.exports = {
-  extractStyle,
 }
